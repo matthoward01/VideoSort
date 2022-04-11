8e2386a92ad470d76f129ca6d43ed224f5edc40f
Matt Howard <matthoward30740@gmail.com>
2022-04-10 20:30:17 -0400

    Bug Fixes and Error Handling...

--- VideoSort/Program.cs
+++ VideoSort/Program.cs
@@ -1,15 +1,17 @@
 ﻿using MediaToolkit;
 using MediaToolkit.Model;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace VideoSort
 {
     class Program
-    {      
-
+    {
+        
         static void Main(string[] Args)
         {
+            List<string> errorList = new List<string>();
             Console.WriteLine("Look in which folder?");
             string inDir = Console.ReadLine();
             Console.WriteLine("----------------------------------------------------------");
@@ -20,8 +22,13 @@ namespace VideoSort
             string[] files = Directory.GetFiles(inDir, "*", SearchOption.AllDirectories);            
             foreach (string f in files)
             {
-                VideoInfo(f, outDir);
+                VideoInfo(f, outDir, errorList);
             }
+            foreach (string e in errorList)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
         }
 
         public static void FolderCreation(string outDir)
@@ -32,6 +39,7 @@ namespace VideoSort
                 Directory.CreateDirectory(Path.Combine(outDir, "848x480"));
                 Directory.CreateDirectory(Path.Combine(outDir, "1920x1080"));
                 Directory.CreateDirectory(Path.Combine(outDir, "1080x1920"));
+                Directory.CreateDirectory(Path.Combine(outDir, "Errors"));
             }
             catch (Exception) { }
         }
@@ -42,45 +50,78 @@ namespace VideoSort
             {
                 if(vm.Height <= 480)
                 {
-                    File.Copy(vm.FilePath, Path.Combine(outDir, "848x480", Path.GetFileName(vm.FilePath)), true);
+                    try
+                    {
+                        File.Copy(vm.FilePath, Path.Combine(outDir, "848x480", Path.GetFileName(vm.FilePath)), true);
+                        Console.WriteLine("Sent to 848x480...");
+                        Console.WriteLine("----------------------------------------------------------");
+                    }
+                    catch (Exception) { }
                 }
                 else
                 {
-                    File.Copy(vm.FilePath, Path.Combine(outDir, "1920x1080",Path.GetFileName(vm.FilePath)), true);
+                    try
+                    {
+                        File.Copy(vm.FilePath, Path.Combine(outDir, "1920x1080",Path.GetFileName(vm.FilePath)), true);
+                        Console.WriteLine("Sent to 1920x1080...");
+                        Console.WriteLine("----------------------------------------------------------");
+                    }
+                    catch (Exception) { }
                 }
             }
             else
             {
                 if (vm.Width <= 480)
                 {
-                    File.Copy(vm.FilePath, Path.Combine(outDir, "480x848",Path.GetFileName(vm.FilePath)), true);
+                    try
+                    {
+                        File.Copy(vm.FilePath, Path.Combine(outDir, "480x848",Path.GetFileName(vm.FilePath)), true);
+                        Console.WriteLine("Sent to 480x848...");
+                        Console.WriteLine("----------------------------------------------------------");
+                    }
+                    catch (Exception) { }
                 }
                 else
                 {
-                    File.Copy(vm.FilePath, Path.Combine(outDir, "1080x1980",Path.GetFileName(vm.FilePath)), true);
+                    try
+                    {
+                        File.Copy(vm.FilePath, Path.Combine(outDir, "1080x1920",Path.GetFileName(vm.FilePath)), true);
+                        Console.WriteLine("Sent to 1080x1920...");
+                        Console.WriteLine("----------------------------------------------------------");
+                    }
+                    catch (Exception) { }
                 }
             }
         }
 
-        public static void VideoInfo(string fileName, string outDir)
+        public static void VideoInfo(string fileName, string outDir, List<string> errorList)
         {
-            VideoModel videoModel = new VideoModel();
-            MediaFile mediaFile = new MediaFile { Filename = fileName };
-
-            using (var engine = new Engine())
+            try
             {
-                engine.GetMetadata(mediaFile);
-            }
-            string[] dimensions = mediaFile.Metadata.VideoData.FrameSize.Split('x');
-            videoModel.FilePath = fileName;
-            videoModel.Width = Int32.Parse(dimensions[0]);
-            videoModel.Height = Int32.Parse(dimensions[1]);
-            Console.WriteLine(videoModel.FilePath);
-            Console.WriteLine(videoModel.Width);
-            Console.WriteLine(videoModel.Height);
-            Console.WriteLine("----------------------------------------------------------");
+                VideoModel videoModel = new VideoModel();
+                MediaFile mediaFile = new MediaFile { Filename = fileName };
 
-            SortVideos(videoModel, outDir);
+                using (var engine = new Engine())
+                {
+                    engine.GetMetadata(mediaFile);
+                }
+                string[] dimensions = mediaFile.Metadata.VideoData.FrameSize.Split('x');
+                videoModel.FilePath = fileName;
+                videoModel.Width = Int32.Parse(dimensions[0]);
+                videoModel.Height = Int32.Parse(dimensions[1]);
+                Console.WriteLine(videoModel.FilePath);
+                Console.WriteLine(videoModel.Width);
+                Console.WriteLine(videoModel.Height);
+
+                SortVideos(videoModel, outDir);
+            }
+            catch(Exception)
+            {
+                errorList.Add(fileName);
+                File.Copy(fileName, Path.Combine(outDir, "Errors", Path.GetFileName(fileName)), true);
+                Console.WriteLine("Sent to Errors...");
+                Console.WriteLine("----------------------------------------------------------");
+            }
         }
 
         public class VideoModel
